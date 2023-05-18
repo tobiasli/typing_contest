22f51213aed14147b6ef8ae15d9b710e74754f7e
Tobias Litherland <tobiaslland@gmail.com>
2023-05-18 23:16:06 +0200

    first version done

---
+++ typing_contest/code/typing_contest.py
@@ -0,0 +1,99 @@
+import typing as ty
+import os
+import sys
+import statistics
+
+import tregex
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def get_contestants_and_filenames(folder: str) -> ty.Dict[str, ty.Dict[str, ty.Any]]:
+    """Return a dictionary of contestants and filename."""
+    return {os.path.splitext(os.path.basename(file))[0]: dict(name=os.path.splitext(os.path.basename(file))[0],
+                                                              filename=os.path.abspath(os.path.join(folder, file))) for
+            file in
+            os.listdir(folder)}
+
+
+def get_file_contents(filename: str) -> str:
+    with open(filename, "r") as file:
+        data = file.readlines()
+        for i, line in enumerate(data):
+            data[i] = data[i].strip("\n")
+    return data
+
+
+def score_two_files(candidate_filename: str, fasit_filename: str) -> ty.Tuple[float, ty.List[float]]:
+    candidate = get_file_contents(candidate_filename)
+    fasit = get_file_contents(fasit_filename)
+
+    scores = list()
+    score = None
+    for i, fasit_line in enumerate(fasit):
+        if len(candidate) - 1 >= i:
+            candidate_line = candidate[i]
+            scores.append(tregex.similarity(candidate_line, fasit_line))
+        else:
+            scores.append(0)
+
+    score = statistics.mean(scores)
+    return score, scores
+
+
+def rank_contestants(contestants: ty.Dict[str, ty.Dict]) -> ty.List[ty.Dict]:
+    iterable = [v for v in contestants.values()]
+
+    return sorted(iterable, key=lambda c: c['score'], reverse=True)
+
+
+def histogram(contestants) -> None:
+    # Creating dataset
+    marks = np.array([c['score'] for c in contestants])
+    labels = [f"{c['name']}" for c in contestants]
+
+    # Creating histogram
+    fig, ax = plt.subplots(1, 1)
+    bars = ax.bar(labels, marks)
+
+    # Add counts above the two bar graphs
+    for rect, label in zip(bars, labels):
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width() / 2.0, height, f'{label}', ha='center', va='bottom')
+
+    ax.get_xaxis().set_visible(False)
+    ax.set_ylim([0, 1])
+
+    # # Set title
+    # ax.set_title("Title")
+    #
+    # # adding labels
+    # ax.set_xlabel('x-label')
+    # ax.set_ylabel('y-label')
+    #
+    # # Make some labels.
+    # labels = [f"{c['name']}" for c in contestants]
+    #
+    # rects = ax.patches
+    # for rect, label in zip(rects, labels):
+    #     height = rect.get_height()
+    #     ax.text(rect.get_x() + rect.get_width() / 2, height + 0.01, label,
+    #             ha='center', va='bottom')
+
+    # Show plot
+    plt.show()
+
+
+if __name__ == "__main__":
+    game_dir = os.environ['typing_contest_game_dir']
+    fasit_filename = os.environ['typing_contest_fasit_filename']
+
+    contestants = get_contestants_and_filenames(game_dir)
+
+    for person in contestants.values():
+        person['score'], person['scores'] = score_two_files(candidate_filename=person['filename'],
+                                                            fasit_filename=fasit_filename)
+
+    ranked = rank_contestants(contestants)
+
+    histogram(ranked)
---
+++ typing_contest/test/test_typing_contest.py
@@ -0,0 +1,89 @@
+import os
+import tempfile
+import pytest
+
+import typing_contest.code.typing_contest as tc
+
+
+class TestBin():
+
+    def __init__(self) -> None:
+        self.dir: tempfile.TemporaryDirectory = None
+
+    def build(self) -> None:
+        self.dir = tempfile.TemporaryDirectory()
+        # Populate bin:
+        for k, v in dict(Charles="Nothing", Benny="""This is contents\nNo it isn't!""").items():
+            with open(os.path.join(self.dir.name, k + '.txt'), 'w') as file:
+                file.write(v)
+
+    def cleanup(self) -> None:
+        if self.dir:
+            self.dir.cleanup()
+
+        self.dir = None
+
+
+class TestFasit():
+
+    def __init__(self) -> None:
+        self.dir: tempfile.TemporaryDirectory = None
+        self.filename: str = None
+
+    def build(self) -> None:
+        self.dir = tempfile.TemporaryDirectory()
+        self.filename = os.path.join(self.dir.name, 'fasit.txt')
+        # Populate fasit:
+        fasit = """This is contents\nYes it is!\nNo it isn't!"""
+        with open(self.filename, 'w') as file:
+            file.write(fasit)
+
+    def cleanup(self) -> None:
+        if self.dir:
+            self.dir.cleanup()
+
+        self.dir = None
+        self.filename = None
+
+
+@pytest.fixture()
+def game_folder() -> str:
+    test_bin = TestBin()
+    test_bin.build()
+    yield test_bin.dir.name
+    test_bin.cleanup()
+
+
+@pytest.fixture()
+def fasit_filename() -> str:
+    test_fasit = TestFasit()
+    test_fasit.build()
+    yield test_fasit.filename
+    test_fasit.cleanup()
+
+
+def test_get_files(game_folder):
+    expected = dict(
+        Benny=os.path.join(game_folder, "Benny.txt"),
+        Charles=os.path.join(game_folder, "Charles.txt"),
+    )
+    assert tc.get_contestants_and_filenames(game_folder) == expected
+
+
+def test_get_fasit(fasit_filename):
+    expected = ["This is contents", "Yes it is!"]
+    assert tc.get_file_contents(fasit_filename) == expected
+
+
+def test_scores(fasit_filename, game_folder) -> None:
+    candidate_filename = tc.get_contestants_and_filenames(game_folder)['Benny']
+    score, scores = tc.score_two_files(candidate_filename=candidate_filename, fasit_filename=fasit_filename)
+    assert score == pytest.approx(0.5454545454545454)
+    assert scores == [pytest.approx(v) for v in [1, 0.6363636363636364, 0]]
+
+
+def test_rank_contestants() -> None:
+    expected = [dict(name='Benny', score=4), dict(name='James', score=2), dict(name='Charles', score=0)]
+    ranked = tc.rank_contestants(dict(Charles=dict(name='Charles', score=0), James=dict(name='James', score=2), Benny=dict(name='Benny', score=4)))
+    assert ranked == expected
+
